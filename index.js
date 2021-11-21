30110657e992b85f240911ce5d34dbe7e62dadb4
Justin Higgins <justin@selfless.io>
2021-11-21 14:43:07 -0800

    (no entrypoint), test workflow echoing input variables from workflow into node docker

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const puppeteer = require('puppeteer');
-
-// NOTE: if you want to don't use github action, pull these from
-// elsewhere
-const USERNAME = secrets.HN_USERNAME;
-const PASSWORD = secrets.HN_PASSWORD;
+//const puppeteer = require('puppeteer');
 
 const {
-  INPUT_POST_TITLE,
-  INPUT_POST_URL,
+  HN_USERNAME,
+  HN_PASSWORD,
+  POST_TITLE,
+  POST_URL,
 } = process.env;
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com/login');
-
-  const userTypeDelay = Math.floor(100 + (Math.random() * 50));
-  const userInputDelay = userTypeDelay * 10;
-  await page.waitForTimeout(userInputDelay);
-
-  await page.type('input[name=acct][type=text]', USERNAME, { delay: userTypeDelay });
-  await page.type('input[type=password]', PASSWORD, { delay: userTypeDelay });
+const login = async (page) => {
+  await page.type('input[name=acct][type=text]', HN_USERNAME, { delay: userTypeDelay });
+  await page.type('input[type=password]', HN_PASSWORD, { delay: userTypeDelay });
   await page.click('input[type=submit][value=login]');
-  await page.waitForTimeout(userInputDelay);
+}
 
+const post = async (page) => {
   await page.goto('https://news.ycombinator.com/submit');
-  await page.waitForTimeout(userInputDelay);
-
-  await page.type('input[name=title][type=text]', INPUT_POST_TITLE, { delay: userTypeDelay });
-  await page.type('input[name=url][type=url]', INPUT_POST_URL, { delay: userTypeDelay });
+  await page.type('input[name=title][type=text]', POST_TITLE, { delay: userTypeDelay });
+  await page.type('input[name=url][type=url]', POST_URL, { delay: userTypeDelay });
   await page.click('input[type=submit][value=submit]');
+}
 
-  await page.waitForTimeout(userInputDelay);
-  await page.screenshot({ path: 'screenshot.png'});
-  await browser.close();
-})();
+try {
+  (async () => {
+    console.log('----------------------------');
+    console.log({
+      HN_USERNAME,
+      HN_PASSWORD,
+      POST_TITLE,
+      POST_URL,
+    });
+
+    /* const browser = await puppeteer.launch();
+     * const page = await browser.newPage();
+     * await page.goto('https://news.ycombinator.com/login');
+
+     * const userTypeDelay = Math.floor(100 + (Math.random() * 50));
+     * const userInputDelay = userTypeDelay * 10;
+     * await page.waitForTimeout(userInputDelay);
+
+     * await login(page);
+     * await page.waitForTimeout(userInputDelay);
+
+     * await post(page);
+     * await page.waitForTimeout(userInputDelay);
+     * await browser.close();
+
+     * console.log(`Successfully submitted ${POST_TITLE}, ${POST_URL}`);
+     * process.exit(0); */
+  })();
+} catch(e) {
+  console.error(e);
+  process.exit(1);
+}
