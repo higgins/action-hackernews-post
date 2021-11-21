165c3238506795816181977a729c48af767b4f3d
Justin Higgins <justin@selfless.io>
2021-11-21 15:50:27 -0800

    fix refs to waits

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ const {
   INPUT_POST_URL,
 } = process.env;
 
+const USER_TYPE_DELAY = Math.floor(100 + (Math.random() * 50));
+const USER_PAGE_WAIT = USER_TYPE_DELAY * 10;
+
 const login = async (page) => {
-  await page.type('input[name=acct][type=text]', INPUT_HN_USERNAME, { delay: userTypeDelay });
-  await page.type('input[type=password]', INPUT_HN_PASSWORD, { delay: userTypeDelay });
+  await page.type('input[name=acct][type=text]', INPUT_HN_USERNAME, { delay: USER_TYPE_DELAY });
+  await page.type('input[type=password]', INPUT_HN_PASSWORD, { delay: USER_TYPE_DELAY });
   await page.click('input[type=submit][value=login]');
 }
 
 const post = async (page) => {
   await page.goto('https://news.ycombinator.com/submit');
-  await page.type('input[name=title][type=text]', INPUT_POST_TITLE, { delay: userTypeDelay });
-  await page.type('input[name=url][type=url]', INPUT_POST_URL, { delay: userTypeDelay });
+  await page.type('input[name=title][type=text]', INPUT_POST_TITLE, { delay: USER_TYPE_DELAY });
+  await page.type('input[name=url][type=url]', INPUT_POST_URL, { delay: USER_TYPE_DELAY });
   await page.click('input[type=submit][value=submit]');
 }
 
@@ -28,15 +31,13 @@ try {
     const page = await browser.newPage();
     await page.goto('https://news.ycombinator.com/login');
 
-    const userTypeDelay = Math.floor(100 + (Math.random() * 50));
-    const userInputDelay = userTypeDelay * 10;
-    await page.waitForTimeout(userInputDelay);
+    await page.waitForTimeout(USER_PAGE_WAIT);
 
     await login(page);
-    await page.waitForTimeout(userInputDelay);
+    await page.waitForTimeout(USER_PAGE_WAIT);
 
     await post(page);
-    await page.waitForTimeout(userInputDelay);
+    await page.waitForTimeout(USER_PAGE_WAIT);
     await browser.close();
 
     console.log(`Successfully submitted ${INPUT_POST_TITLE}, ${INPUT_POST_URL}`);
